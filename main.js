16738b65455eb2728508e8c311fe5541dd493040
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2022-09-18 12:51:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function StartClassification() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/dQrmz-Q7m/model.json", modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
